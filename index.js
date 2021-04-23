a42daa276cd702c44bf954a18bf9be7a104673f8
sam aggrey <samaggrey@hotmail.com>
2021-04-23 11:07:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const Directors = Models.Director;
 //mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 
