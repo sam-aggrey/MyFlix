8969ec7c4d4c9b74019ad42d8bbd5846c4702f71
sam aggrey <samaggrey@hotmail.com>
2021-09-20 08:59:47 +0000

    Update index.js
    Troubleshoot CORS

--- index.js
+++ index.js
@@ -27,17 +27,18 @@ app.use(bodyParser.json());
 
 
 // Listing only allowed domain to be allowed access
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'http://testsite.com', 'https://sammy-flix.herokuapp.com/' ,'https://sam-flix.netlify.app/'];
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));//app.use(cors());
+// let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'http://testsite.com', 'https://sammy-flix.herokuapp.com/' ,'https://sam-flix.netlify.app/'];
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
+app.use(cors());
 
 let auth = require('./auth')(app);
 
